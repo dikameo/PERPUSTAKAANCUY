aa406dfc46b841fd795806aaa807c2af930935b2
MipowGITHUB <mumininsodia@gmail.com>
2024-06-19 06:25:56 +0700

    upload proses backend

--- PerpusRemake/src/main/java/com/main/Main.java
+++ PerpusRemake/src/main/java/com/main/Main.java
@@ -1,7 +1,7 @@
 package com.main;
 
 import java.net.URL;
-
+import com.main.UI.UIManager;
 import com.main.user.Admin;
 import com.main.user.Mahasiswa;
 import javafx.application.Application;
@@ -11,13 +11,33 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static void main(String[] args) {
+        addUser();
+        launch(args);
+    }
+    public static void addUser(){
+//        Mahasiswa mahasiswa1 = new Mahasiswa("AGUS","202310370311080","FT","INFORMATIKA");
+//        Mahasiswa mahasiswa2 = new Mahasiswa("AHMAD","202310370311091","FT","INFORMATIKA");
+//        Mahasiswa mahasiswa3 = new Mahasiswa("Jukii","202310370311071","FT","INFORMATIKA");
+//        Mahasiswa mahasiswa4 = new Mahasiswa("Natsha","202310370311072","FT","INFORMATIKA");
+//        Admin.getMahasiswaData().add(mahasiswa1);
+//        Admin.getMahasiswaData().add(mahasiswa2);
+//        Admin.getMahasiswaData().add(mahasiswa4);
+//        Admin.getMahasiswaData().add(mahasiswa3);
+//        Book book1 = new Book("UMM-202-301","Ilmu Hitam", "Nyi Towok",100);
+//        book1.setCategory("Sejarah");
+//        User.getBookList().add(book1);
+//        book1.setDuration(9);
+//        mahasiswa1.getBorrowedBooks().add(book1);
+    }
+    
+    
 
     @Override
     public void start(Stage primaryStage) {
@@ -120,8 +140,8 @@ private void loginMahasiswa(Stage primaryStage) {
 
     // Mengatur aksi tombol Login
     btn.setOnAction(e -> {
-        String nim = nimTextField.getText();
-        Mahasiswa userMenu = new Mahasiswa(nim); // Inisialisasi objek UserMenu dengan NIM
+        String nimMahasiswa = nimTextField.getText();
+        Mahasiswa userMenu = new Mahasiswa(nimMahasiswa); // Inisialisasi objek UserMenu dengan NIM
         userMenu.showMenu(primaryStage); // Memanggil metode showMenu
     });
 
@@ -197,8 +217,10 @@ private void loginMahasiswa(Stage primaryStage) {
         primaryStage.show();
     }
 
+    public static void addTempMahasiswa
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
+
+
+
 }
